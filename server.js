31db0f651743581c7b04851e37a3939aff9b3f71
ktp242 <ktp242@nyu.edu>
2015-05-27 06:29:24 -0400

    Updated the comments of server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Required modules
 var sys = require("sys"),  
     http = require("http"),  
     url = require("url"),  
@@ -5,6 +6,7 @@ var sys = require("sys"),
     fs = require("fs");
     mime = require("mime");
 
+// The supported extensions of request files
 extensions = {
     ".html" : "text/html",
     ".css" : "text/css",
@@ -16,7 +18,7 @@ extensions = {
     ".ogv" : "video/ogg"
 };
       
-//a helper function to handle HTTP requests
+//HTTP requests
 function requestHandler(req, res) {
     var
     fileName = req.url || 'index.html',
@@ -25,63 +27,49 @@ function requestHandler(req, res) {
     page404 = localFolder + '404.html',
     result = localFolder + 'result.html';
  
-    //do we support the requested file type?
+    //Check if the extension type of the requested file is supported
     if(ext &&!extensions[ext]){
-        //for now just send a 404 and a short message
+        //Send a 404 and a short message if not supported
         res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end("<html><head></head><body>The requested file type is not supported</body></html>");
+        res.end("<html><head></head><body><h1>The requested file type is not supported.</h1></body></html>");
     };
  
-    //call our helper function
-    //pass in the path to the file we want,
-    //the response object, and the 404 page path
-    //in case the requestd file is not found
     getFile((localFolder + fileName),res,result,extensions[ext]);
 };
  
-//step 2) create the server
+//Create the server
 http.createServer(requestHandler)
  
-//step 3) listen for an HTTP request on port 3000
+//Listen for an HTTP request on port 1234
 .listen(1234);
 
-//helper function handles file verification
+//Handle file verification
 function getFile(filePath,res,result,mimeType){
-    //does the requested file exist?
+    //Check if the requested file exist
     fs.exists(filePath,function(exists){
-        //if it does...
         if(exists){
-            //read the file, run the anonymous function
+            //Read the file
             fs.readFile(filePath,function(err,contents){
                 if(!err){
-                    //if there was no error
-                    //send the contents with the default 200/ok header
                     res.writeHead(200,{
                         "Content-type" : mimeType,
                         "Content-Length" : contents.length
                     });
                     res.end(contents);
                 } else {
-                    //for our own troubleshooting
                     console.dir(err);
                 };
             });
         } else {
-            //if the requested file was not found
-            //serve-up our custom 404 page
+            //For the application, lead every page to result.html
             fs.readFile(result,function(err,contents){
-                //if there was no error
                 if(!err){
-                    //send the contents with a 404/not found header 
-                    // res.writeHead(200, {'Content-Type': 'text/html'});
-                    // res.end(contents);
                     res.writeHead(200, {
                         'Location': result, 
                         'Content-Type': 'text/html'
                     });
                     res.end(contents);  
                 } else {
-                    //for our own troubleshooting
                     console.dir(err);
                 };
             });
